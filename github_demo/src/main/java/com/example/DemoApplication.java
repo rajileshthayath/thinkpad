d83f65549a50f2bf5c39741da432fd2dd62a66be
Rajilesh Thayath <rajilesh.thayath@ril.com>
2017-05-02 19:35:48 +0530

    restcontroller changes

--- github_demo/src/main/java/com/example/DemoApplication.java
+++ github_demo/src/main/java/com/example/DemoApplication.java
@@ -4,22 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
+
 	@RequestMapping("/greeting")
 	public SomeClass greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new SomeClass(name);
 	}
-	
+
 	@RequestMapping("/register")
 	public Registration register(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age,
-			@RequestParam(value = "email") String email, @RequestParam(value = "address")String address) {
+			@RequestParam(value = "email") String email, @RequestParam(value = "address") String address) {
 		return new Registration(name, email, address, age);
 	}
 }
