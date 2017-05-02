3fbc8e754b16c6f629f916d32e9a51f47185e2d4
Rajilesh Thayath <rajilesh.thayath@ril.com>
2017-05-02 18:58:23 +0530

    initial commit.

--- github_demo/src/main/java/com/example/DemoApplication.java
+++ github_demo/src/main/java/com/example/DemoApplication.java
@@ -3,16 +3,23 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-public class GithubDemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubDemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 	
-	@RequestMapping("/test")
-	public Object someMappings() {
-		return new Object();
+	@RequestMapping("/greeting")
+	public SomeClass greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new SomeClass(name);
+	}
+	
+	@RequestMapping("/register")
+	public Registration register(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age,
+			@RequestParam(value = "email") String email, @RequestParam(value = "address")String address) {
+		return new Registration(name, email, address, age);
 	}
 }
